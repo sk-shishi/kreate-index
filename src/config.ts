2512aedd6ead33ce69ad542133dcbc93db813273
Umiuma <umiuma@shinka.network>
2023-03-08 23:14:34 +0700

    feat: project moderation alert

--- src/config.ts
+++ src/config.ts
@@ -97,6 +97,9 @@ export const discord = cached(() => {
       "DISCORD_PROJECT_UPDATE_ALERT_CHANNEL_ID"
     ),
     DISCORD_SHINKA_ROLE_ID: requiredEnv("DISCORD_SHINKA_ROLE_ID"),
+    DISCORD_PROJECT_MODERATION_ALERT_CHANNEL_ID: requiredEnv(
+      "DISCORD_PROJECT_MODERATION_ALERT_CHANNEL_ID"
+    ),
   };
 });
 
